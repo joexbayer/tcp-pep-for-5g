4820f2c9a31fa05128367b67c2f0a17ef771ddfb
Joe Bayer <jb71999@gmail.com>
2022-09-12 15:42:37 +0200

    Experimental: Client and server

---
+++ experimental/client.c
@@ -0,0 +1,29 @@
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <netinet/in.h> 
+#include <unistd.h> // for close
+#include <arpa/inet.h>
+
+int main(int argc, char * argv[])
+{
+    int server;
+    struct sockaddr_in s_in;
+    unsigned char byte;
+
+    bzero((char *)&s_in, sizeof(s_in));
+    s_in.sin_family = AF_INET;
+    s_in.sin_addr.s_addr = inet_addr("127.0.0.1");
+    s_in.sin_port = htons(1025);
+
+    server = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if( connect(server, (struct sockaddr*) &s_in, sizeof(s_in)) == -1 ) {
+        return -1;
+    }       
+
+    printf("Connected.\n");
+    close(server);
+    return 0;
+}
+
---
+++ experimental/server.c
@@ -0,0 +1,34 @@
+#include <string.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main(void)
+{
+    int sd, cd;
+    socklen_t size;     
+    unsigned char byte;
+    struct sockaddr_in s_ain, c_ain;    
+
+    sd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    bzero((char *)&s_ain, sizeof(s_ain));
+    s_ain.sin_family = AF_INET;
+    s_ain.sin_addr.s_addr = INADDR_ANY;
+    s_ain.sin_port = htons(1025);
+
+    if(bind(sd, (struct sockaddr *)&s_ain, sizeof(s_ain)) == -1) {
+        return -1;
+    }
+
+    if(listen(sd, 5) == -1) {
+        return -1;
+    }
+
+    while(1) {
+        size = sizeof(c_ain);
+        cd = accept(sd, (struct sockaddr *)&c_ain, &size);
+        printf("Client Connected\n");
+    }
+}
