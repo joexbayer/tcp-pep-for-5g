5d6fbdf2f3e654029b959c0ff84dec4665b38352
Joe Bayer <jb71999@gmail.com>
2022-09-21 15:53:14 +0200

    Working queues

--- experimental/core.c
+++ experimental/core.c
@@ -6,21 +6,54 @@
 #include <linux/module.h> 
 #include <linux/kernel.h> 
 #include <linux/init.h> 
+#include <linux/sched.h>
+#include <linux/workqueue.h>
+#include <linux/slab.h>
 
 #define DRIVER_AUTHOR "Joe Bayer <joeba@uio.no>" 
 #define DRIVER_DESC "Loadable kernel module"
 
 
+/**
+  * PEPDNA uses netfilter hook to connect directly to actual destination.
+  * 
+  */
+
+
+struct pep_connection {
+    struct work_struct task;
+    int lsocket;
+    int rsocket;
+};
+static struct pep_connection* conn;
+
+
+void workqueue_test(struct work_struct* work)
+{
+    struct pep_connection* conn = container_of(work, struct pep_connection, task);
+    printk(KERN_INFO "[PEP] Testing workqueue function %d %d\n", conn->lsocket, conn->rsocket);
+}
+
 static int __init init_core(void)
 {
-    printk(KERN_INFO "Hello, world 4\n");
+
+    conn = kzalloc(sizeof(struct pep_connection), GFP_ATOMIC);
+    conn->lsocket = 1;
+    conn->rsocket = 2;
+
+    INIT_WORK(&conn->task, workqueue_test);
+    schedule_work(&conn->task);
+    
+
+    printk(KERN_INFO "[PEP] Initilized!\n");
     return 0;
 }
 
 
 static void __exit exit_core(void)
 {
-    printk(KERN_INFO "Goodbye, world 4\n");
+    kfree(conn);
+    printk(KERN_INFO "[PEP] exited.\n");
 }
 
 module_init(init_core);
