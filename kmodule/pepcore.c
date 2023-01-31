cff5d695dd1890e8a3119d12b852b692eacdab91
Joe Bayer <jb71999@gmail.com>
2023-01-31 23:01:56 +0100

    pepcore: added tcp send function

--- kmodule/pepcore.c
+++ kmodule/pepcore.c
@@ -45,12 +45,12 @@ struct pep_server server_state;
 LIST_HEAD(pep_tunnels);
 
 
-struct pep_connection* pep_new_connection(unsigned int ip, unsigned short port)
+struct pep_connection* pep_new_connection(struct socket* sock, unsigned int ip, unsigned short port)
 {
         struct pep_connection* conn = kzalloc(sizeof(struct pep_connection), GFP_KERNEL);
         conn->ip = ip;
         conn->port = port;
-
+        conn->sock = sock;
         return conn; 
 }
 
@@ -59,14 +59,84 @@ inline struct pep_tunnel* pep_new_tunnel()
         return (struct pep_tunnel*) kzalloc(sizeof(struct pep_tunnel), GFP_KERNEL);
 }
 
+int pep_tcp_receive(struct socket *sock, u8 buffer, u32 size)
+{
+	struct msghdr msg = {
+		.msg_flags = MSG_DONTWAIT,
+	};
+
+	struct kvec vec;
+	int rc = 0;
+
+	vec.iov_base = buffer;
+	vec.iov_len  = size;
+
+pep_tcp_receive_read_again:
+	printk(KERN_INFO "[PEP] kernel_recvmsg: calling recvmsg \n");
+	rc = kernel_recvmsg(sock, &msg, &vec, 1, vec.iov_len, MSG_DONTWAIT);
+	if (rc > 0)
+	{
+		tlv_print(buffer);
+		return rc;
+	}
+
+	printk(KERN_INFO "[PEP] pep_tcp_receive: no data!\n");
+
+	if(rc == -EAGAIN || rc == -ERESTARTSYS)
+	{
+		goto pep_tcp_receive_read_again;
+	}
+
+	return rc;
+}
+
+int pep_tcp_send(struct socket *sock, u8 buffer, u32 size)
+{
+        struct msghdr msg = {
+                .msg_flags = MSG_DONTWAIT | MSG_NOSIGNAL,
+        };
+        struct kvec vec;
+        int len, written = 0, left = size;
+
+pep_tcp_send_again:
+
+        vec.iov_len = left;
+        vec.iov_base = (char *)buffer + written;
+
+        len = kernel_sendmsg(sock, &msg, &vec, left, left);
+        if((len == -ERESTARTSYS) && (len == -EAGAIN))
+                goto pep_tcp_send_again;
+        if(len > 0)
+        {
+                written += len;
+                left -= len;
+                if(left)
+                        goto pep_tcp_send_again;
+        }
+        return written ? written : len;
+}
+
 void pep_client_receive_work(struct work_struct *work)
 {
         struct pep_tunnel* tun = container_of(work, struct pep_tunnel, forward_work);
 }
 
 void pep_endpoint_receive_work(struct work_struct *work)
-{
+{       
+        int ret;
         struct pep_tunnel* tun = container_of(work, struct pep_tunnel, forward_work);
+
+        unsigned char *buffer = kzalloc(1500, GFP_KERNEL);
+        if (!buffer) {
+                printk(KERN_INFO "[PEP] pep_endpoint_receive_work: out of memory \n");
+                return;
+        }
+
+        ret = pep_tcp_receive(tun->endpoint, buffer, 1500);
+        likely(ret > 0){
+                ret = pep_tcp_send(tun->client, buffer, ret);
+        }
+
 }
 
 static void pep_endpoint_data_ready(struct sock* sk)
@@ -121,42 +191,21 @@ struct socket* pep_endpoint_connect(u32 ip, u16 port)
         return sock;
 }
 
-int pep_tcp_receive(struct socket *sock, u8 buffer, u32 size)
-{
-	struct msghdr msg = {
-		.msg_flags = MSG_DONTWAIT,
-	};
-
-	struct kvec vec;
-	int rc = 0;
-
-	vec.iov_base = buffer;
-	vec.iov_len  = size;
-
-pep_tcp_receive_read_again:
-	printk(KERN_INFO "[PEP] kernel_recvmsg: calling recvmsg \n");
-	rc = kernel_recvmsg(sock, &msg, &vec, 1, vec.iov_len, MSG_DONTWAIT);
-	if (rc > 0)
-	{
-		tlv_print(buffer);
-		return rc;
-	}
-
-	printk(KERN_INFO "[PEP] pep_tcp_receive: no data!\n");
-
-	if(rc == -EAGAIN || rc == -ERESTARTSYS)
-	{
-		goto pep_tcp_receive_read_again;
-	}
-
-	return rc;
-}
-
-void (struct work_struct *work)
+/**
+ * @brief Responsible for accepting a new client and connection to the desired endpoint.
+ * 
+ * @param work 
+ */
+void pep_server_accept_work(struct work_struct *work)
 {
         int rc = 1;
         int ret;
         struct socket* client = NULL;
+        struct socket* endpoint = NULL;
+
+        struct pep_tunnel* tunnel;
+
+        struct sock* client_sk = NULL;
 
         while(rc == 0) {
                 rc = kernel_accept(server_state.server_socket, &client, O_NONBLOCK);
@@ -168,17 +217,33 @@ void (struct work_struct *work)
                 unsigned char *buffer = kzalloc(1500, GFP_KERNEL);
                 if (!buffer) {
                         printk(KERN_INFO "[PEP] pep_server_accept_work: out of memory \n");
+                        sock_release(client);
                         return;
                 }
 
 		ret = pep_tcp_receive(client, buffer, 1500);
                 if(ret <= 0)
                         return;
-                        
                 
-                /* TODO: Create new tunnel with client and endpoint */
+                /**
+                 * It might be smart to create a new work for the endpoint connection
+                 * as we dont want the accept work to become a potential bottleneck.
+                 * 
+                 * But work queueing could also become a overhead.
+                 */
 
-                sock_release(client);
+                /* Setup client and tunnel.. */
+                client_sk = sock->sk;
+                client_sk->sk_reuse = 1;
+
+                /* use our own data ready function */
+                write_lock_bh(&client_sk->sk_callback_lock);
+                client_sk->sk_user_data = sk->sk_data_ready;
+                client_sk->sk_data_ready = &pep_client_data_ready;
+                write_unlock_bh(&client_sk->sk_callback_lock);
+                        
+        
+
                 kfree(buffer);
         }
 }
