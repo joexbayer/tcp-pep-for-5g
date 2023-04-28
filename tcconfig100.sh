3f611166aa1977af75797503a721f52709f02274
Joe Bayer <jb71999@gmail.com>
2023-04-28 15:55:03 +0200

    tc: updated scripts for 8ms and 20ms

--- tcconfig100.sh
+++
@@ -1,8 +0,0 @@
-sudo tc qdisc del dev enp0s9 root;
-sudo tc qdisc add dev enp0s9 root handle 2: netem delay 100ms;
-sudo tc qdisc add dev enp0s9 parent 2: handle 3: htb default 10;
-sudo tc class add dev enp0s9 parent 3: classid 10 htb rate 10Mbit;
-sudo tc qdisc add dev enp0s9 parent 3:10 handle 11: bfifo limit 25000;
-
-#ssleep 4
-#sudo tc qdisc change dev enp0s9 parent 3:10 handle 11: bfifo limit 250000;
