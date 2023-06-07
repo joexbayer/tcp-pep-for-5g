f4fa0cb332f07b76e6a20235c94bb3197500724e
Joe Bayer <jb71999@gmail.com>
2023-06-07 10:21:47 +0200

    PEP buffer size change

--- scripts/tcconfig100ms.sh
+++ scripts/tcconfig100ms.sh
@@ -1,5 +1,5 @@
-sudo tc qdisc del dev enp0s8 root;
-sudo tc qdisc add dev enp0s8 root handle 2: netem delay 100ms;
-sudo tc qdisc add dev enp0s8 parent 2: handle 3: htb default 10;
-sudo tc class add dev enp0s8 parent 3: classid 10 htb rate 100Mbit;
-sudo tc qdisc add dev enp0s8 parent 3:10 handle 11: bfifo limit 1250000;
+sudo tc qdisc del dev enp36s0 root;
+sudo tc qdisc add dev enp36s0 root handle 2: netem delay 100ms;
+sudo tc qdisc add dev enp36s0 parent 2: handle 3: htb default 10;
+sudo tc class add dev enp36s0 parent 3: classid 10 htb rate 100Mbit;
+sudo tc qdisc add dev enp36s0 parent 3:10 handle 11: bfifo limit 1250000;
--- scripts/tcconfig20ms1.sh
+++ scripts/tcconfig20ms1.sh
@@ -1,6 +1,6 @@
-sudo tc qdisc del dev enp0s9 root;
-sudo tc qdisc add dev enp0s9 root handle 2: netem delay 20ms;
-sudo tc qdisc add dev enp0s9 parent 2: handle 3: htb default 10;
-sudo tc class add dev enp0s9 parent 3: classid 10 htb rate 10Mbit;
-sudo tc qdisc add dev enp0s9 parent 3:10 handle 11: bfifo limit 25000;
+sudo tc qdisc del dev enp24s0 root;
+sudo tc qdisc add dev enp24s0 root handle 2: netem delay 20ms;
+sudo tc qdisc add dev enp24s0 parent 2: handle 3: htb default 10;
+sudo tc class add dev enp24s0 parent 3: classid 10 htb rate 10Mbit;
+sudo tc qdisc add dev enp24s0 parent 3:10 handle 11: bfifo limit 25000;
 #sudo tc qdisc add dev enp0s9 parent 3:10 handle 11: fq_codel limit 3+000;
