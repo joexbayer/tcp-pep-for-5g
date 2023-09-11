4ee1c46c76301862d2739b97f36778b461457755
Joe Bayer <jb71999@gmail.com>
2023-09-11 22:01:29 +0200

    small updates

---
+++ scripts/experiments.sh
@@ -0,0 +1,22 @@
+UDP_PACKETS=450
+TRIES=10
+#for (( flows=1; flows <= $TRIES; flows++ )); do
+flows=10
+for (( try=1; try <= $TRIES; try++ )); do
+    sudo echo "Test $try: $flows flows";
+    ssh router "sudo tc class change dev enp24s0 parent 1: classid 11 htb rate 15mbit" > /dev/null;
+    ssh pc04 "cd uio-master-joeba; python2 ultra_ping/echo.py --client 172.16.11.5 --n_packets $(($UDP_PACKETS+($flows*100)))" > /dev/null & 
+    sleep 5;
+
+    for (( flow=1; flow <= $flows; flow++ )); do
+        echo "Starting flow ${flow}"
+        ssh pc04 "cd uio-master-joeba; ./client_file -p" > /dev/null &
+    done
+
+    sleep 13;
+    ssh router "sudo tc class change dev enp24s0 parent 1: classid 11 htb rate 70mbit" > /dev/null;
+    wait
+    ssh pc04 "cd ~/uio-master-joeba;mv udp_packetn_latency_pairs results/udp_${flows}_flow/udp_${flows}_flows_${try}_pep" > /dev/null
+done
+ssh pc05 "cd ~/uio-master-joeba;mv ./logs/server.log results/udp_${flows}_flow_pep.log" > /dev/null
+#done
