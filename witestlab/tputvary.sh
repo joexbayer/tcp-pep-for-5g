da93b2c2b18e8762b78c636bb6b0a351668d6efa
Joe Bayer <jb71999@gmail.com>
2024-02-01 15:50:51 +0100

    More tests, prepare for 5G test and thesis progress.

---
+++ witestlab/tputvary.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+while IFS=, read -r tput tdiff
+do 
+	tdiff=${tdiff::-1}	
+	#ts= `date +%s.%N`
+	echo "$tdiff,$tput"
+	#sudo tc class replace dev $(ip route get 10.0.3.1 | grep -oP "(?<=dev )[^ ]+") parent 1: classid 1:3 htb rate "$tput"mbit
+	sleep $tdiff
+done < "$1"-tput.csv
