2431c60dd2e8d88ef5c5037f5b863237f822c6f9
Joe Bayer <jb71999@gmail.com>
2024-03-05 16:01:06 +0100

    Progress

--- plot-cwnd.py
+++ plot-cwnd.py
@@ -1,81 +1,48 @@
 import matplotlib.pyplot as plt
-import sys
-import datetime
-import numpy as np
 import pandas as pd
-import matplotlib
-matplotlib.use('Agg')
-
+import numpy as np
 
 def plot_tcp_cwnd(pdf_dir=''):
-    for host in [ '.hylia' ]:
-
-        # Load the data from the CSV file
-        filename='router-receiver.dat'
+    for host in ['.hylia']:
+        filename = 'tcp-cwnd.dat'
         data = pd.read_csv(filename, header=None, sep=',',
-                           names=['timestamp',
-                                  'source_ip',
-                                  'source_port',
-                                  'dest_ip',
-                                  'dest_port',
-                                  'cwnd'])
+                           names=['timestamp', 'source_ip', 'source_port',
+                                  'dest_ip', 'dest_port', 'cwnd'])
 
-        # Convert the timestamps to seconds and subtract the first timestamp from all timestamps
         data['timestamp'] = (data['timestamp'] - data['timestamp'].iloc[0])
 
-        # Create a pivot table to separate the data for each flow
         flows = data.pivot_table(values='cwnd',
                                  index='timestamp',
-                                 columns=['source_ip',
-                                          'source_port',
-                                          'dest_ip',
-                                          'dest_port'])
+                                 columns=['source_ip', 'source_port', 'dest_ip', 'dest_port'])
 
-        # Replace zero values with NaN
         flows.replace(0, np.nan, inplace=True)
 
-        # Remove rows with NaN values
-        # flows.dropna(inplace=True)
+        # Set the figure size
+        plt.figure(figsize=(15, 8))  # Adjust the width and height as needed
 
-        
-
-        # Plot the data for all flows in one graph
         ax = flows.plot(linewidth=2, fontsize=20)
-        ax.set_xlabel('Time (s)', fontsize=20)
-        ax.set_ylabel('Congestion Window (pkts.)', fontsize=20)
-
+        # Removed label setting
         ax.grid(True, which='major', lw=0.65, ls='--', dashes=(3, 7), zorder=70)
-
-        # Set the legend outside the graph
-        ax.legend(labels=["Flow 1", "Flow 2", "Flow 3"], fontsize="20")
-
-        # plt.locator_params(axis='x', nbins=11)
-
         plt.yticks(fontsize=18)
         plt.xticks(fontsize=18)
-
-        plt.grid(True, which='major', lw=0.65, ls='--', dashes=(3, 7), zorder=70)
-
         ax.set_ylim(bottom=0)
-
-        # Hide the right and top spines
         ax.spines['right'].set_visible(True)
         ax.spines['top'].set_visible(True)
-
-        # Only show ticks on the left and bottom spines
         ax.yaxis.set_ticks_position('left')
         ax.xaxis.set_ticks_position('bottom')
 
-        # ax.get_legend().remove()
-        # plt.margins(x=0.02)
-        plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1.0)
-        plt.savefig(pdf_dir + '/' + filename + '.pdf',
-                format='pdf',
-                dpi=1200,
-                bbox_inches='tight',
-                pad_inches=0.025)
+        # Improve layout
+        plt.tight_layout()
 
+        # Preview the plot
+        plt.show()
+
+        # Save the plot to a PDF file
+        plt.savefig(pdf_dir + '/' + filename + '.pdf',
+                    format='pdf',
+                    dpi=1200,
+                    bbox_inches='tight',
+                    pad_inches=0.025)
 
 if __name__ == '__main__':
-    # Plot cwnd
     plot_tcp_cwnd(pdf_dir='.')
--- witestlab/scripts/setup.sh
+++ witestlab/scripts/setup.sh
@@ -3,8 +3,13 @@ echo $(ip route get $IP | grep -oP "(?<=dev )[^ ]+");
 sudo tc qdisc del dev $(ip route get $IP | grep -oP "(?<=dev )[^ ]+") root; 
 sudo tc qdisc replace dev $(ip route get $IP | grep -oP "(?<=dev )[^ ]+") root handle 1: htb default 3;
 sudo tc class add dev $(ip route get $IP | grep -oP "(?<=dev )[^ ]+") parent 1: classid 1:3 htb rate 10gbit;
-sudo tc qdisc add dev $(ip route get $IP | grep -oP "(?<=dev )[^ ]+") parent 1:3 handle 3: fq_codel interval 150;
+sudo tc qdisc add dev $(ip route get $IP | grep -oP "(?<=dev )[^ ]+") parent 1:3 handle 3: bfifo limit 7500000;
+#sudo tc qdisc add dev $(ip route get $IP | grep -oP "(?<=dev )[^ ]+") parent 1:3 handle 3: bfifo limit 37500000; # BDP   
+#sudo tc qdisc add dev $(ip route get $IP | grep -oP "(?<=dev )[^ ]+") parent 1:3 handle 3: fq_codel interval 150;
 
 #sudo tc filter add dev 10Gf protocol ip parent 1: prio 1 u32 match ip protocol 17 0xff flowid 1:1;
-#sudo tc qdisc add dev $(ip route get $IP | grep -oP "(?<=dev )[^ ]+") parent 1:3 handle 3: bfifo limit 7500000;
 
+sudo sysctl -w net.core.rmem_max=16777216;
+sudo sysctl -w net.core.wmem_max=16777216;
+sudo sysctl -w net.ipv4.tcp_rmem='4096 87380 16777216';
+sudo sysctl -w net.ipv4.tcp_wmem='4096 65536 16777216';
