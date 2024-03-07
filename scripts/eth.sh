ba4aeb43099b1b4a935f7bb3f77f5ed4f8c44e22
Joe Bayer <jb71999@gmail.com>
2024-03-07 15:42:32 +0100

    More tests and fixing new bugs

--- scripts/eth.sh
+++ scripts/eth.sh
@@ -1,5 +1,5 @@
-  sudo ethtool -K 10Ge tso off;
-  sudo ethtool -K 10Ge gso off;
-  sudo ethtool -K 10Ge lro off;
-  sudo ethtool -K 10Ge gro off;
-  sudo ethtool -K 10Ge ufo off;
+  sudo ethtool -K 10Ge tso on;
+  sudo ethtool -K 10Ge gso on;
+  sudo ethtool -K 10Ge lro on;
+  sudo ethtool -K 10Ge gro on;
+  sudo ethtool -K 10Ge ufo on;
--- witestlab/scripts/setup.sh
+++ witestlab/scripts/setup.sh
@@ -3,9 +3,9 @@ echo $(ip route get $IP | grep -oP "(?<=dev )[^ ]+");
 sudo tc qdisc del dev $(ip route get $IP | grep -oP "(?<=dev )[^ ]+") root; 
 sudo tc qdisc replace dev $(ip route get $IP | grep -oP "(?<=dev )[^ ]+") root handle 1: htb default 3;
 sudo tc class add dev $(ip route get $IP | grep -oP "(?<=dev )[^ ]+") parent 1: classid 1:3 htb rate 3gbit;
-#sudo tc qdisc add dev $(ip route get $IP | grep -oP "(?<=dev )[^ ]+") parent 1:3 handle 3: bfifo limit 7500000;
+sudo tc qdisc add dev $(ip route get $IP | grep -oP "(?<=dev )[^ ]+") parent 1:3 handle 3: bfifo limit 7500000;
 #sudo tc qdisc add dev $(ip route get $IP | grep -oP "(?<=dev )[^ ]+") parent 1:3 handle 3: bfifo limit 37500000; # BDP   
-sudo tc qdisc add dev $(ip route get $IP | grep -oP "(?<=dev )[^ ]+") parent 1:3 handle 3: bfifo limit 50000000; # BDP   
+#sudo tc qdisc add dev $(ip route get $IP | grep -oP "(?<=dev )[^ ]+") parent 1:3 handle 3: bfifo limit 50000000; # BDP   
 #sudo tc qdisc add dev $(ip route get $IP | grep -oP "(?<=dev )[^ ]+") parent 1:3 handle 3: fq_codel interval 150;
 
 #sudo tc filter add dev 10Gf protocol ip parent 1: prio 1 u32 match ip protocol 17 0xff flowid 1:1;
